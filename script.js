9a058706d52b7d4109d0b8bbb1b708b5ee1d3ccc
Huseyin SAHIN <sakarya.hsa54@gmail.com>
2023-03-19 21:53:55 +0300

    general design updates have been made

--- script.js
+++ script.js
@@ -3,49 +3,67 @@ const notes = document.querySelector("#notes");
 
 addButton.addEventListener("click", () => addNewNote());
 
-
 function addNewNote() {
 
     const note = document.createElement("div");
     note.classList.add("note");
 
-    note.innerHTML = `
-            <div class="tools">
-                <img src="fastener.png" alt="">
-                <button class="edit">
-                    <i class="fa-solid fa-pencil"></i>
-                </button>
-                <button class="delete">
-                    <i class="fa-solid fa-eraser"></i>
-                </button>
-            </div>
-            <div class="main hidden">as</div>
-            <textarea></textarea>
+    note.innerHTML = `<div class="note-body">
+                        <div class="tools">
+                            <img src="fastener.png" alt="">
+                            <button class="edit">
+                                <i class="fa-solid fa-pencil"></i>
+                            </button>
+                            <button class="delete">
+                                <i class="fa-solid fa-eraser"></i>
+                            </button>
+                        </div>
+                        <div class="main hidden"></div>
+                        <textarea></textarea>
+                    </div>
     `;
-    deleteFunction(note);
-    editFunction(note)
 
-    notes.appendChild(note);
+    notes.insertAdjacentElement("beforeend", note)
+
+    const editButton = note.querySelector(".edit");
+    const deleteButton = note.querySelector(".delete");
+    const main = note.querySelector(".main");
     const textArea = note.querySelector("textarea");
-    textArea.focus()
+
+    editButton.addEventListener("click", () => editFunction(main, textArea));
+    textAreaBlur(note, main, textArea)
+    deleteButton.addEventListener("click", () => {
+        deleteFunction(note, main);
+    });
+
 }
 
-function deleteFunction(e) {
-    const deleteButton = e.querySelector(".delete");
-    deleteButton.addEventListener("click", () => e.remove());
+function editFunction(main, textArea) {
+    main.classList.toggle("hidden")
+    textArea.classList.toggle("hidden")
+    textArea.focus();
 }
 
-function editFunction(e) {
+function textAreaBlur(note, main, textArea) {
 
-    const editButton = e.querySelector(".edit");
-    const main = e.querySelector(".main");
-    const textArea = e.querySelector("textarea");
-
-    editButton.addEventListener("click", () => {
-        main.classList.toggle("hidden")
-        textArea.classList.toggle("hidden")
-        textArea.focus();
-
-        main.innerHTML = textArea.value
+    textArea.focus();
+    textArea.addEventListener("blur", () => {
+        editFunction(main, textArea)
+        main.innerHTML = textArea.value.replace(/\n/g, "<br>")
+        if (main.textContent === "") {
+            deleteFunction(note, main)
+        }
     })
+}
+
+function deleteFunction(note, main) {
+    note.classList.add("delete")
+    addButton.disabled = true;
+    addButton.style.opacity = "0.8";
+    main.style.width = window.getComputedStyle(main).width;
+    setTimeout(() => {
+        note.remove();
+        addButton.disabled = false;
+        addButton.style.opacity = "1";
+    }, 500);
 }

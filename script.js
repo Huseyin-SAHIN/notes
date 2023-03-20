d3c8c28f75b13d814bf2c001bc41d53d0895281d
Huseyin SAHIN <sakarya.hsa54@gmail.com>
2023-03-20 19:09:13 +0300

    added local storage

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const addButton = document.querySelector("#add");
 const notes = document.querySelector("#notes");
+const storedNotes = JSON.parse(localStorage.getItem("notes"));
 
-addButton.addEventListener("click", () => addNewNote());
+addButton.addEventListener("click", () => createNewNote());
 
-function addNewNote() {
+if (storedNotes) {
+    storedNotes.forEach((note) => {
+        createNewNote(note);
+    })
+}
+
+function createNewNote(noteContent = "") {
 
     const note = document.createElement("div");
     note.classList.add("note");
@@ -18,52 +25,73 @@ function addNewNote() {
                                 <i class="fa-solid fa-eraser"></i>
                             </button>
                         </div>
-                        <div class="main hidden"></div>
+                        <div class="main hidden">${noteContent}</div>
                         <textarea></textarea>
                     </div>
     `;
 
-    notes.insertAdjacentElement("beforeend", note)
+    notes.insertAdjacentElement("beforeend", note);
 
     const editButton = note.querySelector(".edit");
     const deleteButton = note.querySelector(".delete");
     const main = note.querySelector(".main");
     const textArea = note.querySelector("textarea");
 
-    editButton.addEventListener("click", () => editFunction(main, textArea));
-    textAreaBlur(note, main, textArea)
-    deleteButton.addEventListener("click", () => {
-        deleteFunction(note, main);
+    editButton.addEventListener("click", () => {
+        toggleNoteEditMode(main, textArea);
+        textArea.focus();
     });
 
+    saveNoteOnBlur(note, main, textArea);
+
+    deleteButton.addEventListener("click", () => {
+        deleteNote(note, main);
+    });
+
+
+    if (main.innerHTML === "") {
+        textArea.focus();
+    } else {
+        textArea.value = main.innerHTML
+        toggleNoteEditMode(main, textArea);
+    }
 }
 
-function editFunction(main, textArea) {
-    main.classList.toggle("hidden")
-    textArea.classList.toggle("hidden")
-    textArea.focus();
+function toggleNoteEditMode(main, textArea) {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
 }
 
-function textAreaBlur(note, main, textArea) {
-
-    textArea.focus();
+function saveNoteOnBlur(note, main, textArea) {
     textArea.addEventListener("blur", () => {
-        editFunction(main, textArea)
+        toggleNoteEditMode(main, textArea);
         main.innerHTML = textArea.value.replace(/\n/g, "<br>")
+        updateLocalStorage();
         if (main.textContent === "") {
-            deleteFunction(note, main)
-        }
-    })
-}
+            deleteNote(note, main);
+        };
+    });
+};
 
-function deleteFunction(note, main) {
-    note.classList.add("delete")
+function deleteNote(note, main) {
+    note.classList.add("delete");
     addButton.disabled = true;
     addButton.style.opacity = "0.8";
     main.style.width = window.getComputedStyle(main).width;
     setTimeout(() => {
         note.remove();
+        updateLocalStorage();
         addButton.disabled = false;
         addButton.style.opacity = "1";
     }, 500);
+}
+
+function updateLocalStorage() {
+    const notesText = document.querySelectorAll(".main")
+    const notesArray = [];
+    notesText.forEach((note) => {
+        notesArray.push(note.innerHTML);
+    });
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
 }
